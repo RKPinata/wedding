c68033bb2b26b59fc0a45b006458469d080e61f7
teukurian <teukurian090598@gmail.com>
2024-09-30 15:17:55 +0800

    Complete gift popup

--- src/components/Popup/PopupItems/GiftPopup/GiftPopup.tsx
+++ src/components/Popup/PopupItems/GiftPopup/GiftPopup.tsx
@@ -1,9 +1,18 @@
 import styles from './GiftPopup.module.css'
+import BankDetails from './GiftPopupItems/BankDetails/BankDetails';
+import Mailing from './GiftPopupItems/Mailing/Mailing';
 
-const GiftPopup = () => {
+const GiftPopup: React.FC = () => {
     return (
         <div className={styles["gift-popup-container"]}>
-            <p>this will show gifts</p>
+            <div className="gift-popup-items">
+                <div className={styles["bank-details-container"]}>
+                    <Mailing />
+                </div>
+                <div className={styles["mailing-address-container"]}>
+                    <BankDetails />
+                </div>
+            </div>
         </div>
     )
 }
