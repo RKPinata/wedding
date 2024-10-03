ecdd194348be6a9e266037b576b3974e2a5f6911
teukurian <teukurian090598@gmail.com>
2024-10-03 21:24:10 +0800

    add animation

--- src/components/BottomOverlay/BottomOverlay.tsx
+++ src/components/BottomOverlay/BottomOverlay.tsx
@@ -1,25 +1,25 @@
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 import Popup from '../Popup/Popup';
 import Tabbar from '../Tabbar/Tabbar';
 import styles from './BottomOverlay.module.css';
+import ContactPopup from '../Popup/PopupItems/ContactPopup/ContactPopup';
+import GiftPopup from '../Popup/PopupItems/GiftPopup/GiftPopup';
+import LocationPopup from '../Popup/PopupItems/LocationPopup/LocationPopup';
+import Rsvp from '../Popup/PopupItems/RSVP/Rsvp';
 
 const BottomOverlay = () => {
-  const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
-  const [selectedComponent, setSelectedComponent] = useState<ReactNode | null>(null);
+  const [openPopup, setOpenPopup] = useState<string | null>(null);
 
-  const handleSelect = (title: string, component: ReactNode) => {
-    if (selectedTitle === title) {
-      setSelectedTitle(null);
-      setSelectedComponent(null);
-    } else {
-      setSelectedTitle(title);
-      setSelectedComponent(component);
-    }
+  const handleSelect = (title: string) => {
+    setOpenPopup((prevTitle) => (prevTitle === title ? null : title));
   };
 
   return (
     <div className={styles["bottom-overlay-container"]}>
-      <Popup title={selectedTitle} component={selectedComponent} />
+      <Popup title="Contact" isOpen={openPopup === 'Contact'} component={<ContactPopup />} />
+      <Popup title="Location" isOpen={openPopup === 'Location'} component={<LocationPopup />} />
+      <Popup title="Gift" isOpen={openPopup === 'Gift'} component={<GiftPopup />} />
+      <Popup title="RSVP" isOpen={openPopup === 'RSVP'} component={<Rsvp />} />
       <Tabbar onSelect={handleSelect} />
     </div>
   );
--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,18 +1,26 @@
-import { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import styles from "./Popup.module.css";
 
-const Popup: React.FC<{ title: string | null, component: ReactNode }> = ({ title, component }) => {
-  if (!title || !component) return null;
+const Popup: React.FC<{ title: string; component: ReactNode; isOpen: boolean }> = ({ title, component, isOpen }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    } else {
+      setTimeout(() => setIsVisible(false), 400);
+    }
+  }, [isOpen]);
+
+  if (!isVisible && !isOpen) return null;
 
   return (
-    <div className={styles["popup-container"]}>
+    <div className={`${styles["popup-container"]} ${isOpen ? styles["open"] : styles["close"]}`}>
       <div className={styles["popup-items"]}>
         <div className={styles["popup-title"]}>
           <h2>{title}</h2>
         </div>
-        <div className={styles["popup-content"]}>
-          {component ? component : <p>No component loaded</p>}
-        </div>
+        <div className={styles["popup-content"]}>{component}</div>
       </div>
     </div>
   );
