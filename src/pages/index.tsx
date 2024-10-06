27f040d984bac4e4d235aa49b5605b5700d9f16b
teukurian <teukurian090598@gmail.com>
2024-10-06 21:16:28 +0800

    remove local font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,8 @@
-import localFont from "next/font/local";
 import CardLayout from "@/components/CardLayout/CardLayout";
-
-const playfairDisplay = localFont({
-  src: "./fonts/PlayfairDisplay-VariableFont_wght.ttf",
-  variable: "--playfai-display",
-  weight: "400",
-})
-
-// const weddingFont = localFont({
-//   src: "./fonts/theweddingsignatureregular-bwz55.ttf",
-//   variable: "--font-wedding",
-//   weight: "400",
-// });
 
 export default function Home() {
   return (
-    <main className={`${playfairDisplay.variable}`}>
+    <main>
       <CardLayout />
     </main>
   );
