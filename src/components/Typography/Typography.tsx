c75220e638afb286febf95f97ec8a47866cca79d
teukurian <teukurian090598@gmail.com>
2024-10-08 22:19:59 +0800

    Create typography component

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -3,15 +3,27 @@ interface TypographyProps {
     className?: string;
     color?: string;
     align?: 'left' | 'center' | 'right';
-    font?: 'Playfair Display' | 'Gladis' | 'BP Black White';
+    font?: 'Playfair Display' | 'Gladis' | 'BP Black White' | 'Meie Script' | 'Madjestic Comfort Serif';
+    fontSize?: string;
     children: React.ReactNode;
 }
 
-const Typography: React.FC<TypographyProps> = ({ variant = 'p', className = '', color, align = 'center', font = 'Playfair Display', children }) => {
+const Typography: React.FC<TypographyProps> = ({ 
+    variant = 'p', 
+    className = '', 
+    color='black', 
+    align = 'center', 
+    font = 'Playfair Display', 
+    fontSize, 
+    children 
+}) => {
     const Component = variant;
 
     return (
-        <Component style={{ color, textAlign: align, fontFamily: font }} className={className}>
+        <Component 
+            style={{ color, textAlign: align, fontFamily: font, fontSize: fontSize }}
+            className={className}
+        >
             {children}
         </Component>
     );
