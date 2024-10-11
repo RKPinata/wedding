f269df063181ffe2da31599f1b4d6a846b79eed5
teukurian <teukurian090598@gmail.com>
2024-10-11 22:57:36 +0800

    Add tentative component to card content

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -1,6 +1,7 @@
 import Welcome from '../Welcome/Welcome'
 import Invitation from '../Invitation/Invitation';
 import Styles from './CardContent.module.css'
+import Tentative from '../Tentative/Tentative';
 
 const CardContent: React.FC = () => {
     return (
@@ -8,6 +9,7 @@ const CardContent: React.FC = () => {
             <div className={Styles["card-content-items"]}>
                 <Welcome />
                 <Invitation />
+                <Tentative />
             </div>
         </div>
     )
