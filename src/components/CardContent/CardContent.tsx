4de6618615a8de52cee7c498831d67b95fbc8238
teukurian <teukurian090598@gmail.com>
2024-10-11 20:09:10 +0800

    Add invitation to card content

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -1,4 +1,5 @@
 import Welcome from '../Welcome/Welcome'
+import Invitation from '../Invitation/Invitation';
 import Styles from './CardContent.module.css'
 
 const CardContent: React.FC = () => {
@@ -6,6 +7,7 @@ const CardContent: React.FC = () => {
         <div className={Styles["card-content-container"]}>
             <div className={Styles["card-content-items"]}>
                 <Welcome />
+                <Invitation />
             </div>
         </div>
     )
