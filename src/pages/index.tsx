3587cf5ebcdfe61c9d9f9323ea2cd1c1d9e55a52
TeukuRyanIskandar <teukurian090598@gmail.com>
2024-10-28 15:25:16 +0800

    Add toaster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import CardLayout from "@/components/CardLayout/CardLayout";
+import { Toaster } from "sonner";
 
 export default function Home() {
   return (
     <main>
       <CardLayout />
+      <Toaster
+        position="top-center" 
+        toastOptions={{
+          style: {
+            justifyContent: "center",
+            fontFamily: 'Kathya Script',
+            backgroundColor: '#c9a1db4d',
+            fontSize: '1.5rem'
+          }
+        }}        
+      />
     </main>
   );
 }
