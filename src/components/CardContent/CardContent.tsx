bfc9c8f2e8b8a50e0199552408091f6aca80065a
TeukuRyanIskandar <teukurian090598@gmail.com>
2024-10-28 10:35:44 +0800

    Remove unsused var

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect } from 'react';
 import Welcome from '../Welcome/Welcome';
 import Invitation from '../Invitation/Invitation';
 import Styles from './CardContent.module.css';
-import Tentative from '../Tentative/Tentative';
 import Doa from '../Doa/Doa';
 import Wishes from '../Wishes/Wishes';
 import Music from '../Music/Music';
